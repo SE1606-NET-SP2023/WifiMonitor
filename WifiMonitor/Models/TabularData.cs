e98a81d15575e9c50074e20fe35906acf5174c13
Việt Nguyễn <nviet0139@gmail.com>
2023-03-24 03:06:49 +0700

    TabularData
    Có gì thì sửa giúp tôi

---
+++ WifiMonitor/Models/TabularData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WifiMonitor.Models
+{
+    public class TabularData
+    {
+        public TimeOnly? Time { get; set; }
+        public string? Signal { get; set; }
+        public string? Channel { get; set; }
+        public string? SecurityMode { get; set; }
+
+    }
+}
---
+++ WifiMonitor/Views/TabularData.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WifiMonitor.ViewModels;
+
+namespace WifiMonitor.Views
+{
+    /// <summary>
+    /// Interaction logic for TabularData.xaml
+    /// </summary>
+    public partial class TabularData : UserControl
+    {
+        public TabularData()
+        {
+            InitializeComponent();
+            lv.ItemsSource = TabularDataView.list;
+        }
+
+        private void lv_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
