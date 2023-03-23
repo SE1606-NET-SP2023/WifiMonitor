bd951c74d68eb1a37da8f148c01f92d6ccd48e79
DatLT279 <datlt279@gmail.com>
2023-03-23 23:03:34 +0700

    Add library, enitities

---
+++ WifiMonitor/Models/AccessPointSnapshot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WifiMonitor.Models
+{
+    public class AccessPointSnapshot
+    {
+        public string? SSID { get; set; }
+        public string? BSSID { get; set; }
+        public string? Signal { get; set; }
+        public string? Percentage { get; set; }
+        public string? Band { get; set; }
+        public string? Channel { get; set; }
+        public string? Width { get; set; }
+        public string? Security { get; set; }
+        public string? Mode { get; set; }
+        public string? Color { get; set; }
+        public TimeOnly? DetectedTime { get; set; }
+    }
+}
