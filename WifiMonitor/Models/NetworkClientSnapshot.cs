c023a344f3cd37e57967845b685f9dfc99fc43ef
DatLT279 <datlt279@gmail.com>
2023-03-25 11:12:29 +0700

    Update network Scanner

---
+++ WifiMonitor/Models/NetworkClientSnapshot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WifiMonitor.Models
+{
+    public class NetworkClientSnapshot
+    {
+        public string? IPAddress { get; set; }
+        public string? DeviceName { get; set; }
+        public string? MACAddress { get; set; }
+        public string? Vendor { get; set; }
+        public string? DeviceInfo { get; set; }
+        public string? UserName { get; set; }
+        public DateTime? DetectedTime { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
